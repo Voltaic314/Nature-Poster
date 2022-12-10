e0c56a622cc4854df17ad988e7d425960f17024a
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2022-12-10 18:32:10 -0500

    Making the code actually work! Poggers!
    Also now it uploads videos in Title case at least.

--- Nature_Poster_Videos.py
+++ Nature_Poster_Videos.py
@@ -18,7 +18,6 @@ import os  # needed to get the file paths
 import random  # Used to pick a random keyword to search when grabbing a video
 from pexels_api import API  # need this to get images to use from Pexels (our source of images for the project)
 from datetime import datetime  # used for date and time in the FB log posting, so we know when things were posted to FB
-import facebook  # to add captions and comments to existing posts.
 import json  # to decipher the dictionary we get back in return from FB servers. (Mainly this is used to edit captions to the posts).
 import sqlite3  # our database access, where all the posts get logged to, a local DB file on the Raspberry Pi.
 
@@ -52,9 +51,11 @@ def get_file_size(url):
     requests_content_length = requests.get(url)
 
     # divides file size by 1000, so we can get how many kilobytes it is
-    length = float(requests_content_length.headers.get('content-length')) / 1000
-
-    return length
+    length_as_str = requests_content_length.headers.get('content-length')
+    if length_as_str:
+        return float(length_as_str) / 1_000_000
+    else:
+        return None
 
 
 def acceptable_extension(video_extension):
@@ -71,18 +72,26 @@ def acceptable_extension(video_extension):
     return any(extensions in video_extension for extensions in extensions)
 
 
-def post_to_fb(video_link):
+def post_to_fb(video_link, video_description, video_permalink):
     """
-    This function posts to fb given a specific video.url that
-    you wish to use.
+    This function posts to fb given a specific video.url that you wish to use.
+
     :param video_link: any url of a pexels video, must end in .mp4 or something similar.
+    :param video_description: the description attribute of the video class instance, ideally like the alt txt of the vid
+    :param video_permalink: the permalink of the video that links back to the original Pexels post.
     :returns: response from FB servers with the post id or an error
     """
 
     fb_page_id = "101111365975816"
-    post_url = f'https://graph-video.facebook.com/v15.0/{fb_page_id}/videos'
+    post_url = f'https://graph.facebook.com/{fb_page_id}/videos'
+    GitHub_Link = 'https://github.com/Voltaic314/Nature-Poster'
+    message = f'Description: {video_description.title()}\n\nPexels link: {video_permalink}\n\n' \
+              f'P.S. This Facebook post was created by a bot. To learn more about how it works,' \
+              f' check out the GitHub page here: {GitHub_Link}'
     payload = {
-        "url": video_link,
+        "file_url": video_link,
+        "title": video_description.title(),
+        "description": message,
         "access_token": config.config_stuff['FB_Access_Token']
     }
 
@@ -106,31 +115,6 @@ def get_post_id_from_json(request):
 
     else:
         return None
-
-
-def edit_fb_post_caption(post_id, video_description, video_permalink):
-    """
-    This function takes a given FB post and edits a caption to it.
-
-    :param post_id: any FB post you wish to edit.
-    :param video_description: what you wish to edit to it, preferably a str type
-    :param video_permalink: the link of the original pexels video for credit
-    :returns: None
-    """
-
-    fb_page_id = "101111365975816"
-    GitHub_Link = 'https://github.com/Voltaic314/Nature-Poster'
-
-    # define fb variable for next line with our access info
-    fb = facebook.GraphAPI(access_token=config.config_stuff['FB_Access_Token'])
-
-    # edit caption of existing fb post we just made
-    fb.put_object(parent_object=f'{fb_page_id}_{post_id}', connection_name='',
-                  message=f'Description: {video_description}\n\nPexels image link: {video_permalink}\n\n'
-                          f'P.S. This Facebook post was created by a bot. To learn more about how it works,'
-                          f' check out the GitHub page here: {GitHub_Link}')
-
-    print("Caption has been edited to post successfully.")
 
 
 def id_is_in_db(table, id_string):
@@ -171,7 +155,7 @@ def log_to_DB(formatted_tuple: tuple):
     :param formatted_tuple: tuple containing the info that the user wishes to log to the database
     :returns: None
     """
-    cursor.execute('INSERT INTO Nature_Bot_Logged_FB_Posts VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', formatted_tuple)
+    cursor.execute('INSERT INTO Nature_Bot_Logged_FB_Posts_Videos VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', formatted_tuple)
 
 
 def process_videos(videos):
@@ -193,16 +177,16 @@ def process_videos(videos):
         video_permalink = video.url
         video_extension = video.extension
         video_link = video.link
-        video_file_size = get_file_size(video.url)
+        video_file_size = get_file_size(video.link)
 
         if not acceptable_extension(video_extension):
             continue
 
-        if id_is_in_db('Nature_Bot_Logged_FB_Posts', str(video.id)):
+        if id_is_in_db('Nature_Bot_Logged_FB_Posts_Videos', str(video.id)):
             continue
 
         # make sure the file size is less than 1 GB. (This is primarily for FB posting limitations).
-        if video_file_size >= 1_000_000:
+        if video_file_size >= 1000:
             continue
 
         # If the video is greater than 20 minutes long, start over. (also for FB Positing limitations)
@@ -212,12 +196,17 @@ def process_videos(videos):
         if not no_badwords(video_description.lower().split(" ")):
             continue
 
-        post_to_fb_request = post_to_fb(video_link)
+        post_to_fb_request = post_to_fb(video_link, video.description, video_permalink)
         fb_post_id = get_post_id_from_json(post_to_fb_request)
-        successful_post = fb_post_id in post_to_fb_request
+        # successful_post = fb_post_id in post_to_fb_request
 
-        if not successful_post:
-            continue
+        if not fb_post_id:
+            print("Post was not successful")
+            print(post_to_fb_request)
+            print(f"Your video link was: {video.link}")
+            print(f"The file size of this video was {video_file_size} MB.")
+            data_to_log = "error raised"
+            break
 
         else:
 
@@ -225,14 +214,10 @@ def process_videos(videos):
 
             dt_string = str(datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
 
-            edit_fb_post_caption(fb_post_id, video_description, video_permalink)
-
-            print("Caption has been edited successfully.")
-
             data_to_log = (
                 dt_string, str(post_to_fb_request), str(video_description), str(video_user),
-                str(video_id), str(video_permalink), str(video.url), str(video_link),
-                float(video_file_size),
+                str(video_id), searched_term, int(video.duration), str(video_permalink),
+                str(video_link), float(video_file_size),
             )
 
             log_to_DB(data_to_log)
@@ -257,7 +242,7 @@ def main():
     :returns: None
     """
 
-    global done, connect, cursor
+    global done, connect, cursor, searched_term
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     db_path = os.path.join(BASE_DIR, "Nature_Bot_Data.db")
     connect = sqlite3.connect(db_path)
@@ -265,7 +250,8 @@ def main():
     PEXELS_API_KEY = config.config_stuff3['PEXELS_API_KEY']
     api = API(PEXELS_API_KEY)
     Search_Terms = get_search_terms()  # list of art sources to use from Pexels
-    api.search_video(str(random.choice(Search_Terms)), page=1, results_per_page=15)
+    searched_term = str(random.choice(Search_Terms))
+    api.search_video(searched_term, page=1, results_per_page=15)
 
     done = False
     while not done:
