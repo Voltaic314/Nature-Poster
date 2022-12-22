a1ab9cd3c32c63607f76764689462d687153cc9f
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-12-21 21:02:56 -0500

    Starting the progress on working on OOP implementation for this script
    Creating a database.py script that we can reference for database utilities. I'll try to make this as reusable of code as I can but some of these methods will naturally just be related to the nature poster project as a whole. I'll work on it though.

---
+++ database.py
@@ -0,0 +1,11 @@
+"""
+Author: Logan Maupin
+
+The purpose of this python script is to store our methods for sending and retrieving data to our database.
+This way we can store the methods here and then call them later in other projects.
+
+The overall goal is to use OOP techniques to create clean, testable, and reusable code.
+"""
+
+import sqlite3
+
